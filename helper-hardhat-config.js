63c742d1ea4f8b2c1e56d48d3d1f4e78582eb1c2
JW <digitalhumanlife@gmail.com>
2023-05-08 19:32:08 +0900

    first erc20

---
+++ helper-hardhat-config.js
@@ -0,0 +1,41 @@
+const networkConfig = {
+    31337: {
+        name: "localhost",
+        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+        mintFee: "10000000000000000", // 0.01 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    5: {
+        name: "goerli",
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        subscriptionId: "10434",
+        callbackGasLimit: "500000", // 500,000
+        mintFee: "10000000000000000", // 0.01 ETH,
+        interval: "30",
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+    },
+    // Price Feed Address, values can be obtained at https://docs.chain.link/data-feeds/price-feeds/addresses
+    11155111: {
+        name: "sepolia",
+        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        callbackGasLimit: "5000000", // 5,000,000 gas
+        mintFee: "10000000000000000", // 0.01 ETH
+        subscriptionId: "1023", // add your ID here!
+    },
+}
+
+const DECIMALS = "18"
+const INITIAL_PRICE = "200000000000000000000"
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_PRICE,
+}
